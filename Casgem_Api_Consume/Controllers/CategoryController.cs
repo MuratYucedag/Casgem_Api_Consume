8ef35f01a4c78647c8d80b9a59395a65c58f7beb
MuratYucedag <yucedagmurat23@gmail.com>
2023-07-19 14:10:03 +0300

    Add project files.

---
+++ Casgem_Api_Consume/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_Api_Consume.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
